aa67ee616828791b506e21433fc4e7d8f85945e3
Asindu De Silva <asindudesilva@gmail.com>
2025-03-19 14:29:02 +0530

    init

---
+++ src/main/java/com/example/firmnet/FirmnetApplication.java
@@ -0,0 +1,13 @@
+package com.example.firmnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firmnet/FirmnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firmnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
